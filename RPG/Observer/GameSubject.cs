2dacf9cf84b7de5fe9dd10233ecd62140f97943b
Luana Seres Girotto <luana.sgirotto@gmail.com>
2024-12-01 18:49:33 -0300

    fix: atualizado gameSubject

--- RPG/Observer/GameSubject.cs
+++ RPG/Observer/GameSubject.cs
@@ -1,23 +1,37 @@
-using System.Collections.Generic;
 using RPG.Characters;
+using System;
+using System.Collections.Generic;
 
-namespace RPG.Observer
+namespace RPG
 {
-  public class GameSubject
-  {
-    private readonly List<IObserver> _observers = new List<IObserver>();
-
-    public void AddObserver(IObserver observer)
+    public class GameSubject
     {
-      _observers.Add(observer);
-    }
+        // Lista de observadores que implementam a interface IObserver<BaseCharacter>
+        private readonly List<IObserver<BaseCharacter>> _observers = new();
 
-    public void Notify(BaseCharacter character)
-    {
-      foreach (var observer in _observers)
-      {
-        observer.Update(character);
-      }
+        // Método para adicionar um observador
+        public void RegisterObserver(IObserver<BaseCharacter> observer)
+        {
+            _observers.Add(observer);
+        }
+
+        // Método para notificar os observadores
+        public void NotifyObservers(BaseCharacter character)
+        {
+            foreach (var observer in _observers)
+            {
+                observer.OnNext(character);
+            }
+        }
+
+        // Método para simular uma mudança no estado do personagem
+        public void ChangeCharacterState(BaseCharacter character)
+        {
+            // Exemplo de alteração no personagem (dano, ataque, etc.)
+            character.TakeDamage(10); // Dando um dano de exemplo
+
+            // Após a alteração, notifique os observadores
+            NotifyObservers(character);
+        }
     }
-  }
 }
