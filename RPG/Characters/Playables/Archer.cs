ed2516ab5b6d0161b0aba5a08913ca5267280f10
Luana Seres Girotto <luana.sgirotto@gmail.com>
2024-12-01 18:50:46 -0300

    feat: adicionado takeDamage

--- RPG/Characters/Playables/Archer.cs
+++ RPG/Characters/Playables/Archer.cs
@@ -5,19 +5,25 @@ namespace RPG.Characters.Playables
     public class Archer : BaseCharacter
     {
         public Archer(int health, int defense, int attackPower)
-            : base(health, defense, attackPower, "Goblin")
+            : base(health, defense, attackPower)
         {
+        }
+
+        public override void TakeDamage(int damage)
+        {
+            int damageTaken = Math.Max(0, damage - Defense);
+            Health -= damageTaken;
+            Console.WriteLine($"{GetType().Name} recebeu {damageTaken} de dano. Saúde restante: {Health}");
+            NotifyObservers();
         }
 
         public override void PerformAttack(BaseCharacter target)
         {
             bool isCriticalHit = new Random().Next(0, 100) < 25; // 25% chance de crítico
             int damage = isCriticalHit ? AttackPower * 2 : AttackPower;
-
-            Console.WriteLine($"{GetType().Name} dispara {(isCriticalHit ? "um golpe crítico" : "uma flecha")} causando {damage} de dano!");
             target.TakeDamage(damage);
 
-            NotifyObservers();
+            Console.WriteLine($"{GetType().Name} dispara {(isCriticalHit ? "um golpe crítico" : "uma flecha")} causando {damage} de dano!");
         }
     }
 }
