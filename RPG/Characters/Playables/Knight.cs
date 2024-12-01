b84d7e4d86bdb15094f13ea48d37d97424947e09
Luana Seres Girotto <luana.sgirotto@gmail.com>
2024-12-01 18:50:35 -0300

    feat: adicionado performAttack

--- RPG/Characters/Playables/Knight.cs
+++ RPG/Characters/Playables/Knight.cs
@@ -5,18 +5,29 @@ namespace RPG.Characters.Playables
     public class Knight : BaseCharacter
     {
         public Knight(int health, int defense, int attackPower)
-            : base(health, defense, attackPower, "Orc")
+            : base(health, defense, attackPower)
         {
         }
 
         public override void TakeDamage(int damage)
         {
-            int heavyArmorReduction = 10; // Exemplo de redução adicional para o Cavaleiro
-            int damageTaken = Math.Max(0, damage - (Defense + heavyArmorReduction));
+            int heavyArmorReduction = 10;
+            int totalDefense = Defense + heavyArmorReduction;
+            int damageTaken = Math.Max(1, damage - totalDefense);
+
             Health -= damageTaken;
 
             Console.WriteLine($"{GetType().Name} recebeu {damageTaken} de dano após proteção pesada. Saúde restante: {Health}");
             NotifyObservers();
+        }
+
+
+
+        public override void PerformAttack(BaseCharacter target)
+        {
+            int damage = AttackPower; // Ataque normal do cavaleiro
+            target.TakeDamage(damage); // Aplica o dano no alvo
+            Console.WriteLine($"{GetType().Name} ataca com um golpe pesado causando {damage} de dano!");
         }
     }
 }
