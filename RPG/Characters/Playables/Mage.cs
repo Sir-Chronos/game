0a61ea5276346d21eb7bdeb2737be2a5e719d95e
Luana Seres Girotto <luana.sgirotto@gmail.com>
2024-12-01 18:50:32 -0300

    feat: adicionado performAttack

--- RPG/Characters/Playables/Mage.cs
+++ RPG/Characters/Playables/Mage.cs
@@ -5,18 +5,26 @@ namespace RPG.Characters.Playables
     public class Mage : BaseCharacter
     {
         public Mage(int health, int defense, int attackPower)
-            : base(health, defense, attackPower, "Necromancer")
+            : base(health, defense, attackPower)
         {
         }
 
         public override void TakeDamage(int damage)
         {
-            int magicShield = 5; // Exemplo de escudo mágico que reduz dano fixo
+            int magicShield = 5;
             int damageTaken = Math.Max(0, damage - (Defense + magicShield));
             Health -= damageTaken;
 
             Console.WriteLine($"{GetType().Name} recebeu {damageTaken} de dano após escudo mágico. Saúde restante: {Health}");
             NotifyObservers();
+        }
+
+        public override void PerformAttack(BaseCharacter target)
+        {
+            int magicDamage = AttackPower * 2; // Ataque mágico do mago
+            target.TakeDamage(magicDamage);  // Aplica o dano mágico no alvo
+
+            Console.WriteLine($"{GetType().Name} lança um feitiço causando {magicDamage} de dano!");
         }
     }
 }
